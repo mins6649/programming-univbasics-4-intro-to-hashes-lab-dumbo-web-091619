a3aece0be06645563a44207b9c976977283cf1a1
Minyoung Shin <mins6649@gmail.com>
2021-01-27 20:44:38 -0500

    commit tree 974d8b3a2b6a5a34b8ff65c956b47f3912079175

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -11,6 +11,7 @@ describe "working with hashes" do
     end
 
 
+
     describe "my_hash" do
       it "creates and returns a valid hash that contains key/value pairs of your choice" do
         expect(my_hash).to be_a(Hash)
